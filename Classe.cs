efaa521caaf4f996cbd836b98034fa5ed04ddfa3
K3R0 <johann.ly@outlook.fr>
2024-10-21 15:06:45 +0200

    Ajout classe Eleve et Classe

---
+++ Classe.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    internal class Classe
+    {
+        public string nomClasse {  get; private set; }
+        public List<Eleve> eleves;
+        public List<string> matieres;
+        //Constructeur
+        public Classe(string nomClasse) 
+        { 
+            this.nomClasse = nomClasse;
+            eleves = new List<Eleve>();
+            matieres = new List<string>();
+        }
+        //Ajout d'un eleve a la classe
+        public void ajouterEleve(string prenom, string nom)
+        {
+            if (this.eleves.Count < 30)
+            {
+                this.eleves.Add(new Eleve(prenom, nom));
+            }
+            else
+            {
+                throw new Exception("Une classe ne peut pas avoir plus de 30 eleves");
+            }
+        }
+        //Ajout d'une matiere
+        public void ajouterMatiere(string matiere)
+        {
+            if (this.matieres.Count < 10)
+            {
+                this.matieres.Add(matiere);
+            }
+            else
+            {
+                throw new Exception("Une classe ne peut pas enseigner plus de 10 matieres");
+            }
+        }
+        //Calcul de la moyenne de chaque eleve sur une matiere
+        public double moyenneMatiere(int i)
+        {
+            double total = 0;
+            foreach (Eleve eleve in this.eleves)
+            {
+                total += eleve.moyenneMatiere(i);
+            }
+            return Math.Round(total/eleves.Count, 2);
+        }
+        //Calcul de la moyenne des moyennes generales de chaque eleves
+        public double moyenneGeneral()
+        {
+            double total = 0;
+            foreach (Eleve eleve in this.eleves)
+            {
+                total += eleve.moyenneGeneral();
+            }
+            return Math.Round(total/ eleves.Count, 2);
+        }
+
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Xml.Serialization;
+
+namespace HNI_TPmoyennes
+{
+    internal class Eleve
+    {
+        public string prenom {  get; private set; }
+        public string nom { get; private set; }
+        public List<Note> notes { get; private set; }
+        //Constructeur
+        public Eleve(string prenom, string nom)
+        {
+            this.prenom = prenom;
+            this.nom = nom;
+            notes = new List<Note>();
+        }
+        //Ajout d'une Note
+        public void ajouterNote(Note note)
+        {
+            if (notes.Count <= 200)
+            {
+                this.notes.Add(note);
+            } else {
+                throw new Exception("Un eleve ne peut pas avoir plus de 200 notes");
+                    }
+        }
+        //Calcul de la moyenne d'une matière
+        public double moyenneMatiere(int i)
+        {
+            float total = 0;
+            int count = 0;
+            foreach (Note note in notes)
+            {
+                if (note.matiere == i)
+                {
+                    total += note.note;
+                    count++;
+                }
+            }
+            return Math.Round(total / count, 2);
+        }
+        //Methode pour recuperer le nombre de matière que un eleve suit
+        private int nombreMatiere()
+        {
+            int max = 0;
+            foreach (Note note in notes)
+            {
+                if (note.matiere > max)
+                {
+                    max = note.matiere;
+                }
+            }
+            return max;
+        }
+        //Calcul de la moyenne des moyennes de chaque matières
+        public double moyenneGeneral()
+        {
+            int nombreMatiere = this.nombreMatiere();
+            double total = 0;
+            for (int i = 0; i < nombreMatiere; i++)
+            {
+                total += this.moyenneMatiere(i);
+            }
+            return Math.Round(total / nombreMatiere, 2);
+        }
+    }
+}
